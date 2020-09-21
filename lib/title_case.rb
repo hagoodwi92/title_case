21993f307c31e0107754b851fb5ae91913d66d6a
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-21 11:09:46 -0700

    Pass test for spec 2: Ignore articles

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -2,7 +2,11 @@ def title_case (string)
   string_lower_case = string.downcase()
   string_array = string_lower_case.split()
   string_array.each do |word|
-    word.capitalize!()
+    if (word == "a") | (word == "an") | (word == "the") | (word == "and")
+      next
+    else 
+      word.capitalize!()
+    end
   end
   return string_array.join(" ")
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,11 +2,11 @@ require ('rspec')
 require('title_case')
 
 describe ('#title_case') do
-  it('Returns a given string in title case') do
-    expect(title_case("once upon a time")).to(eq("Once Upon A Time"))
-  end
+  # it('Returns a given string in title case') do
+  #   expect(title_case("once upon a time")).to(eq("Once Upon A Time"))
+  # end
   it("returns given string in titlecase, excluding articles from titlecase, ") do
-    expect(title_case("once upon a time")).to(eq("Once Upon a Time"))
+    # expect(title_case("once upon a time")).to(eq("Once Upon a Time"))
     expect(title_case("over the hills and far away")).to(eq("Over the Hills and Far Away"))
   end
 end
